c0045d4340c9351ca41199fd076a81ab2948a2d8
kramer <kramer@vbox.vbox>
2025-04-03 08:15:31 -0400

    hello.py

---
+++ hello.py
@@ -0,0 +1,24 @@
+#This line is running python code without a variable
+print("hello word")
+print('I am learning python')
+
+#Creating a program with a variable
+first_name ="Daniel "
+last_name = "Iroh"
+
+#concatenation: combination of two or more variables
+print(first_name + last_name)
+print(first_name +  " " + last_name)
+
+#types of variables
+#integer (0-9)
+#floating-point(float):decimal numbers (e.g 0.5 0.1 9.9)
+#string(str):text data ("hello""python")
+#boolean(bool): true/false values 
+
+age = 18
+sentence = " the legal age to vote in"
+country = "Gabon"
+
+full_sentence = sentence + " " + country
+print(full_sentence + " is " + str(age))
